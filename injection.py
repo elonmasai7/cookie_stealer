cb5e89ae0eaaa7ff0b06e0547d957e26be6fc0e2
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-09-23 22:46:24 +0300

    Create injection.py
    cookie injection

---
+++ injection.py
@@ -0,0 +1,12 @@
+def do_POST(self):
+    # Parse POST data
+    content_length = int(self.headers.get('Content-Length'))
+    post_data = self.rfile.read(content_length)
+
+    # Inject malicious cookies
+    malicious_cookies = http.cookies.SimpleCookie()
+    malicious_cookies['malicious_cookie'] = 'malicious_value'
+    self.send_response(200)
+    self.send_header('Set-Cookie', malicious_cookies.output(header=''))
+    self.end_headers()
+    self.wfile.write(b'Cookie injected!')
